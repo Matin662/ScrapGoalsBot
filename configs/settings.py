8103beebc0470cdb931031200dc4ad9be5d13f1d
Matin662 <113793600+Matin662@users.noreply.github.com>
2025-11-30 03:37:28 +0330

    Update settings.py

--- configs/settings.py
+++ configs/settings.py
@@ -1,1 +1,16 @@
 
+BOT_TOKEN = "8512947553:AAErv0r57WfVdQykkM9vx2t_unAMp80KCUU"  # توکن رباتت
+ADMIN_ID = 7816017310  # یوزر آیدی خودت (عدد)
+
+# اکانت‌های توییتر برای اسکریپ کردن (قابل اضافه شدن توسط ادمین)
+TWITTER_ACCOUNTS = [
+    "centerofgoals"
+]
+
+# اینستنس‌های Nitter (برای پایداری بیشتر چندتا گذاشتم)
+NITTER_INSTANCES = [
+    "https://nitter.net",
+    "https://nitter.privacydev.net",
+    "https://nitter.1d4.us",
+    "https://nitter.space",
+]
